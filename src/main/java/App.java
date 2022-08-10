c94d682c2581e8273b9a11b41818370628b6a3ef
xdial <xdial@mail.ru>
2022-08-10 18:53:15 +0300

    Добавлен класс Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+                System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+                System.out.println(bean.getMessage());
+
+        System.out.println(bean == bean2);
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat == cat1);
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    private int id;
+    public Cat(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
